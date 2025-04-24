2747e36bb82b695039c54948a8fb2e4cc2dc393e
Scott Ratigan <Scott_Ratigan@intuit.com>
2025-04-24 16:57:51 -0700

    fixes simulated error

--- server/src/network-simulator.ts
+++ server/src/network-simulator.ts
@@ -4,11 +4,10 @@ import { RequestHandler } from "express";
 export const networkSimulator: RequestHandler = (req, res, next) => {
   // Do not modify this function. Instead, handle errors caused here on the front end.
   if (process.env.TEST) return next(); // don't mess up local testing
-  console.log("process.env.TEST:", process.env.TEST);
   const start = performance.now();
-  if (Math.random() < 0.1) {
+  if (Math.random() < 0.2) {
     console.error("Something went wrong! Returning 500 error. (This is a simulated error.)");
-    return res.status(500).send("Server Error");
+    return res.status(500).send({ error: "Server Error" });
   }
   while (performance.now() - start < 250) {
     // Simulate an expensive calculation
